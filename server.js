f61dfc74e95268a40bf0710284c0c3427ec3958a
toasterbath0758 <toasterbath0758@gmail.com>
2026-06-09 13:18:07 +0200

    component sort function van maandrankings #6

--- server.js
+++ server.js
@@ -15,6 +15,41 @@ app.engine('liquid', engine.express());
 app.set('views', './views')
 
 
+// load de home
+app.get('/', async function (request, response) {
+// Haal de data uit de directus API 
+   let URLgolfers = 'https://fdnd-agency.directus.app/items/into_golf_golfers'
+   let URLrondes = 'https://fdnd-agency.directus.app/items/into_golf_rounds'
+
+   if(request.query.sort!==undefined){
+      URLgolfers = URLgolfers+"?sort="+request.query.sort
+   }
+
+   if(request.query.filter!==undefined) {
+      URLrondes = URLrondes+"?filter="+request.query.filter
+   }
+
+   const golferResponse = await fetch(URLgolfers)
+   const rondesResponse = await fetch(URLrondes)
+
+   // En haal daarvan de JSON op
+   const golferResponseJSON = await golferResponse.json()
+   const rondesResponseJSON = await rondesResponse.json()
+
+   response.render('index.liquid', { golfers: golferResponseJSON.data, rondes: rondesResponseJSON.data }) 
+    console.log(request.query, rondesResponseJSON.data)
+})
+
+
+// // load de home
+// app.get('/', async function (request, response) {
+// // Haal de data uit de directus API op
+//       const handicapResponse = await fetch(`/items/into_golf_rounds?sort=handicap`)
+//    // En haal daarvan de JSON op
+//    const golferResponseJSON = await rondesResponse.json()
+//    response.render('index.liquid', { handicaps: golferResponseJSON.data }) 
+    
+// })
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)

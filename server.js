a6591386c4e97860ec03ecc2f0bf6eab070a5a99
toasterbath0758 <toasterbath0758@gmail.com>
2026-06-09 13:01:03 +0200

    server.js setup #2

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+
+app.set('views', './views')
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+   // Toon een bericht in de console en geef het poortnummer door
+   console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+

5cf24a0763709f118ba757f70c3a805e66eb345c
toasterbath0758 <toasterbath0758@gmail.com>
2026-06-16 17:20:55 +0200

    route opzetten post gedeelte (recente-rondes) #3

--- server.js
+++ server.js
@@ -125,6 +125,38 @@ app.get('/golfer/:id', async function (request, response) {
 //    response.render('golfer.liquid', { golfers: golferResponseJSON.data, rondes: rondesResponseJSON.data }) 
 //     console.log(request.query, rondesResponseJSON.data)
 // })
+
+
+
+
+/////////////////////////////////////////////////
+// POST + DELETE GEDEELTE
+
+// POST RECENTE RONDES
+app.post('/golfer/:id/score', async function (req, res){
+  const id = req.params.id
+
+
+  try {
+    await fetch('https://fdnd-agency.directus.app/items/into_golf_rounds', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        golfer_id: id,
+        date: req.body.date,
+        course: req.body.course,
+        differential: req.body.differential,
+        type: req.body.type
+      })
+    })
+    res.redirect(303, `/golfer/${id}?status=success`)
+  } catch (error) {
+    res.redirect(303, `/golfer/${id}?status=error`)
+  }
+})
+
+
+
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)

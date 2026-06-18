0ba8c4523f08b8f7736b84fef9e23893f2959d30
kevin.kevin <toasterbath0758@gmail.com>
2026-06-18 14:43:47 +0200

    server setup #2

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-import express from 'express'
 
-import { Liquid } from 'liquidjs';
+import { Liquid } from "liquidjs";
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 const engine = new Liquid();
-app.engine('liquid', engine.express());
+app.engine("liquid", engine.express());
 
-app.set('views', './views')
+app.set("views", "./views");
 
 app.get("/", async function (request, response) {
   const apiResponse = await fetch(
@@ -229,11 +228,10 @@ app.post('/golfer/:id/score', async function (req, res){
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start Express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-   // Toon een bericht in de console en geef het poortnummer door
-   console.log(`Application started on http://localhost:${app.get('port')}`)
-})
-
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
